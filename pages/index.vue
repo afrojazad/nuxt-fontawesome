5efc1f59a51e65c7e0651acd878634007511fd7a
Md Afroj Al Azad <afrojazad@gmail.com>
2022-04-07 06:53:12 +0600

    Fontawesome 6
    Fontawesome 6 configuration in Nuxtjs

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <h1>FontAwesome</h1>
+    <font-awesome-icon icon="fa-solid fa-clipboard-user" class="fa-5x" />
+    <font-awesome-icon icon="fa-solid fa-circle-user" style="color:red; font-size:30px " />
+    <font-awesome-icon icon="fa-regular fa-circle-user" />
+    <font-awesome-icon icon="fa-solid fa-user-doctor" />
+    <font-awesome-icon icon="fa-regular fa-clipboard" />
+    <font-awesome-icon icon="fa-solid fa-clipboard" />
+    <font-awesome-icon icon="fa-brands fa-facebook" />
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
